c7ec6c450eda0912aa75ec81f92c1917682c9e68
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-01-11 16:01:27 +0200

    Был сделан восьмой скрипт

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
+const inputEmailEl = document.querySelector('input[name="email"]');
+const inputPasswordEl = document.querySelector('input[name="password"]');
 
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if( inputEmailEl.value === '' || inputPasswordEl.value === ''){
+        alert('Все поля должны быть заполнены!');
+    } 
+    
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+
+    const formInfo = {
+        email,
+        password,
+    };
+    console.log(formInfo);
+
+    event.currentTarget.reset();
+
+
+});
