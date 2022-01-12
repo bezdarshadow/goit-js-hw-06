a75053da1589f81286fb04856e6da86ee6a75e28
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-01-12 11:25:06 +0200

    Переделал шестой скрипт

--- js/task-06.js
+++ js/task-06.js
@@ -6,10 +6,10 @@ inputValidationEl.addEventListener('input', event => {
 });
 
 inputValidationEl.addEventListener('blur', event => {
-    if(event.currentTarget.value.length < Number(event.currentTarget.dataset.length)){
-        event.currentTarget.classList.add('invalid');
-    } else {
+    if(event.currentTarget.value.length === Number(event.currentTarget.dataset.length)){
         event.currentTarget.classList.add('valid');
+    } else {
+        event.currentTarget.classList.add('invalid');
     }
 });
 
